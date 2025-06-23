21cc77a2b7f72c93bc057ae0b976b5df16613cce
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2025-06-23 11:21:41 +0300

    fix: solve 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (repeatText, repeats) {
+  let result = '';
+  if (repeats < 0) {
+    return 'ERROR';
+  } else {
+    for (let i = 0; i < repeats; i++) {
+      result = result.concat(repeatText);
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
