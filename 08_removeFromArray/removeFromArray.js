c4be6089f886e3837370deb912b66f2188b7ca0c
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2025-07-19 10:39:30 +0300

    fix: solve 08_removeFromArray
    Used the array method `filter` to filter the original array checking if the value exists or not in the array using the `.includes` method.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  // [1,2,3,4]
+  return arr.filter((value) => !args.includes(value));
 };
 
 // Do not edit below this line
