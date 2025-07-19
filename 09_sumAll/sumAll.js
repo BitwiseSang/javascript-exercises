d57b240f998aca24f5cd320d0d5a6720e55afb3b
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2025-07-19 11:42:29 +0300

    fix: solve 09_sumAll
    This was a bit challenging but very rewarding. I failed multiple times before it just clicked.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function (...nums) {
+  let sum = 0;
+  // Type checks
+  filteredNums = nums.filter(
+    (num) => isNum(num) && isPositiveNum(num) && !isFloat(num)
+  );
 
+  if (filteredNums.length !== 2) {
+    return 'ERROR';
+  }
+
+  // Sort filtered array
+  filteredNums.sort((a, b) => a - b);
+  const [start, end] = filteredNums;
+
+  // Loop through and calculate sum
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+function isNum(n) {
+  return typeof n === 'number' && !isNaN(n) && Number(n) === n;
+}
+
+function isPositiveNum(n) {
+  return n > 0;
+}
+
+function isFloat(n) {
+  return typeof n === 'number' && Number(n) === n && n % 1 !== 0;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
